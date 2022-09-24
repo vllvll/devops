0f38cfcae8403c11d1ea03cedb2179f418a5ed74
vllvll <do.code@ya.ru>
2022-09-24 17:23:17 +0300

    Increment 19

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"text/template"
 	"time"
 
 	conf "github.com/vllvll/devops/internal/config"
@@ -15,7 +16,29 @@ import (
 	"github.com/vllvll/devops/internal/types"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+const BuildTemplate = `
+Build version: {{if .version}}{{ .version }}{{ else }}N/A{{ end }}
+Build date: {{if .date}}{{ .date }}{{ else }}N/A{{ end }}
+Build commit: {{if .commit}}{{ .commit }}{{ else }}N/A{{ end }}
+`
+
 func main() {
+	t := template.Must(template.New("build").Parse(BuildTemplate))
+	err := t.Execute(os.Stdout, map[string]string{
+		"version": buildVersion,
+		"date":    buildDate,
+		"commit":  buildCommit,
+	})
+	if err != nil {
+		log.Fatalf("Error with config: %v", err)
+	}
+
 	config, err := conf.CreateAgentConfig()
 	if err != nil {
 		log.Fatalf("Error with config: %v", err)
